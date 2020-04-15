65c3dda9c6caa0f17fb93d4dbb6bc864d0c26a5a
闫方敏(10031602) <yanfm@glodon.com>
2020-04-15 18:25:31 +0800

    update

--- constant/index.jsx
+++ constant/index.jsx
@@ -34,7 +34,7 @@ class Index extends React.Component {
 
   render() {
     {
-      return <div onBlur={this.clickTap.bind(this, 123)}><div className="vvv">123</div></div>;
+      return <div onBlur={this.clickTap}><div className="vvv">123</div></div>;
     }
   }
 
